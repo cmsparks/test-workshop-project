aeeb0a61db1c91cc4592acfbbf58ba75b20767e5
Christian Sparks <csparks@cloudflare.com>
2024-08-21 00:50:07 -0500

    Stub out TradingCardManager class

---
+++ packages/workers-library/src/card-manager.ts
@@ -0,0 +1,44 @@
+import { Ai, type KVNamespace } from '@cloudflare/workers-types'
+
+export type Card = {
+	title: string
+	description: string
+
+	// raw image data in the PNG format
+	imageData: Uint8Array
+}
+
+/**
+ * Example class that wrapps Cloudflare KV
+ */
+export default class TradingCardManager {
+	constructor(
+		public kvBinding: KVNamespace,
+		public aiBinding: Ai
+	) {}
+
+	/**
+	 * @param title title of the trading card
+	 * @param description description of the card
+	 * @returns fully populated card data
+	 */
+	async generateCard(title: string, description: string): Promise<Card> {
+		throw new Error('unimplemented')
+	}
+
+	/**
+	 * @param card card data to save to KV
+	 * @returns card key in KV
+	 */
+	async saveCard(card: Card): Promise<string> {
+		throw new Error('unimplemented')
+	}
+
+	/**
+	 * @param cardKey card key to get
+	 * @returns card from cardKey
+	 */
+	async getCard(cardKey: string): Promise<Card> {
+		throw new Error('unimplemented')
+	}
+}
