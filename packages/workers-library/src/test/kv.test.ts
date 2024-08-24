1ccd3ec3d4b91d36f3bc1d960e44071e400eead5
Dario Piotrowicz <dario@cloudflare.com>
2024-08-24 12:23:56 +0100

    format all files

--- packages/workers-library/src/test/kv.test.ts
+++ packages/workers-library/src/test/kv.test.ts
@@ -1,31 +1,31 @@
-import KV from '../kv'
-import { describe, expect, it } from 'vitest'
-import { env } from 'cloudflare:test'
+import KV from '../kv';
+import { describe, expect, it } from 'vitest';
+import { env } from 'cloudflare:test';
 
 describe('test KV wrapper class', () => {
 	it('KV.setFoo()', async () => {
-		const kv = new KV(env.KV)
+		const kv = new KV(env.KV);
 
-		await kv.setFoo('testValue')
-		expect(await env.KV.get('foo')).toBe('testValue')
-	})
+		await kv.setFoo('testValue');
+		expect(await env.KV.get('foo')).toBe('testValue');
+	});
 
 	it('KV.getFoo()', async () => {
-		const kv = new KV(env.KV)
+		const kv = new KV(env.KV);
 
-		const shouldBeNull = await kv.getFoo()
-		expect(shouldBeNull).toBeNull()
+		const shouldBeNull = await kv.getFoo();
+		expect(shouldBeNull).toBeNull();
 
-		await env.KV.put('foo', 'test')
-		const shouldBeTest = await kv.getFoo()
-		expect(shouldBeTest).toBe('test')
-	})
+		await env.KV.put('foo', 'test');
+		const shouldBeTest = await kv.getFoo();
+		expect(shouldBeTest).toBe('test');
+	});
 
 	it('repeatedly get/set', async () => {
-		const kv = new KV(env.KV)
+		const kv = new KV(env.KV);
 
-		await kv.setFoo('newTest')
-		const newTest = await kv.getFoo()
-		expect(newTest).toBe('newTest')
-	})
-})
+		await kv.setFoo('newTest');
+		const newTest = await kv.getFoo();
+		expect(newTest).toBe('newTest');
+	});
+});
